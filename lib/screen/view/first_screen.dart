3d5f820411b2165767769798861a37a36bf8ebe4
ASUS <devikhirpara61@gmail.com>
2023-04-28 09:55:14 +0530

    3

--- lib/screen/view/first_screen.dart
+++ lib/screen/view/first_screen.dart
@@ -8,64 +8,67 @@ class SliderPattern extends StatefulWidget {
 }
 
 class _SliderPatternState extends State<SliderPattern> {
-  double Sliderpattern = 1;
-  String Pattern = '';
-
-  void _generatePattern(double value) {
-    String pattern = '';
-    for (int i = 1; i <= value; i++) {
-      pattern += '${List.generate(i, (j) => j + 1).join()} \n';
-    }
-    setState(() {
-      Pattern = pattern;
-    });
-  }
+  double S1 = 1;
+  String P1 = '1';
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar( backgroundColor: Colors.grey,
-        title: Text(
-          "Slider Pattern",
-          style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
-        ),
-        centerTitle: true,
-
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Slider(
-            inactiveColor: Colors.grey.shade300,
-            value: Sliderpattern,
-            min: 1,
-            max: 5,
-            activeColor: Colors.grey,
-            divisions: 4,
-            label: Sliderpattern.round().toString(),
-            onChanged: (value) {
-              setState(() {
-                Sliderpattern = value;
-              });
-            },
-            onChangeEnd: _generatePattern,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Text(
+            "Slider Pattern",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
           ),
-          const SizedBox(height: 20),
-          Container(
-            height: 250,
-            width: 250,
-            color: Colors.grey.shade400,
-            child: Center(
-              child: Text(
-                Pattern,
-                style: TextStyle(fontSize: 25),
-                textAlign: TextAlign.center,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Slider(
+              inactiveColor: Colors.grey.shade300,
+              value: S1,
+              min: 1,
+              max: 5,
+              activeColor: Colors.grey,
+              divisions: 4,
+              label: S1.round().toString(),
+              onChanged: (value) {
+                setState(() {
+                  S1 = value;
+                });
+              },
+              onChangeEnd: _generatePattern,
+            ),
+            SizedBox(height: 30),
+            Container(
+              height: 250,
+              width: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.grey.shade400,
+              ),
+              child: Center(
+                child: Text(
+                  P1,
+                  style: TextStyle(fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
+  }
+
+  void _generatePattern(double value) {
+    String p2 = '';
+    for (int i = 1; i <= value; i++) {
+      p2 += "${List.generate(i, (j) => j + 1).join()} \n";
+    }
+    setState(() {
+      P1 = p2;
+    });
   }
 }
